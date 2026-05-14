ee542c4ddcec4b4d6ec049fea3cb906013afbf85
scriptedbyshivam <shivam2024maurya@gmail.com>
2026-05-14 08:02:55 +0530

    add style in website

---
+++ script.js
@@ -0,0 +1,39 @@
+let container = document.querySelector("#cardsection");
+let cardsection = document.querySelector(".card");
+
+const cardWidth = cardsection.offsetWidth + 60;
+let scroll = cardWidth * 4;
+let btnleft = document.querySelector(".swapbtnLeft");
+btnleft.addEventListener("click", function () {   
+        container.scrollLeft -= scroll;
+});
+
+
+let btnright = document.querySelector(".swapbtnRight");
+btnright.addEventListener("click", function () {
+    container.scrollLeft += scroll;
+});
+
+window.addEventListener('resize', () => {
+  const isMobile = window.innerWidth <= 767;
+  scroll = isMobile ? cardWidth : cardWidth * 4;
+});
+
+
+
+
+let faq = document.querySelectorAll(".faq");
+
+faq.forEach(function(faq){
+    faq.addEventListener("click",function(){
+        
+        let para = faq.nextElementSibling;
+        if(para.style.display === "block"){
+            para.style.display = "none";
+        }else{
+            para.style.display = "block";
+
+        }
+    });
+
+});
